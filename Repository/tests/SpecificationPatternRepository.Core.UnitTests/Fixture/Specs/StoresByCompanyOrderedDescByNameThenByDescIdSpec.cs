86b80a709247c0dfd4740206342b3c07642da7ff
Marcelo Rodrigues <lrodrigues.marcelo@gmail.com>
2023-05-12 18:40:05 -0300

    SpecificationBuilderTests: OrderByDescThenByDesc

---
+++ Repository/tests/SpecificationPatternRepository.Core.UnitTests/Fixture/Specs/StoresByCompanyOrderedDescByNameThenByDescIdSpec.cs
@@ -0,0 +1,14 @@
+﻿using SpecificationPatternRepository.Core.UnitTests.Fixture.Entities;
+
+namespace SpecificationPatternRepository.Core.UnitTests.Fixture.Specs
+{
+    public class StoresByCompanyOrderedDescByNameThenByDescIdSpec : BaseSpecification<Store>
+    {
+        public StoresByCompanyOrderedDescByNameThenByDescIdSpec(int companyId)
+        {
+            SpecificationBuilder.Where(store => store.CompanyId == companyId)
+                .OrderByDescending(store => store.Name)
+                .ThenByDescending(store => store.Id);
+        }
+    }
+}
---
+++ Repository/tests/SpecificationPatternRepository.Core.UnitTests/SpecificationBuilderTests/OrderByDescThenByDesc.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using SpecificationPatternRepository.Core.Expressions;
+using SpecificationPatternRepository.Core.UnitTests.Fixture.Specs;
+
+namespace SpecificationPatternRepository.Core.UnitTests.SpecificationBuilderTests
+{
+    public class OrderByDescThenByDesc
+    {
+        [Fact]
+        public void AppendsOrderExpressionToListWithThenByDescendingType_GivenThenByDescendingExpression()
+        {
+            StoresByCompanyOrderedDescByNameThenByDescIdSpec spec = 
+                new StoresByCompanyOrderedDescByNameThenByDescIdSpec(1);
+
+            spec.OrderByClauses.Should().HaveCount(2);
+            spec.OrderByClauses.First().OrderType.Should().Be(OrderByType.OrderByDescending);
+            spec.OrderByClauses.Last().OrderType.Should().Be(OrderByType.ThenByDescending);
+        }
+    }
+}
