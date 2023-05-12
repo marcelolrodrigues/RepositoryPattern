324aaef4a81fe997e90bd88fd6941d682847e085
Marcelo Rodrigues <lrodrigues.marcelo@gmail.com>
2023-05-12 18:20:36 -0300

    SpecificationBuilderTests: OrderByDesc

---
+++ Repository/tests/SpecificationPatternRepository.Core.UnitTests/SpecificationBuilderTests/OrderByDesc.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using SpecificationPatternRepository.Core.Expressions;
+using SpecificationPatternRepository.Core.UnitTests.Fixture.Specs;
+
+namespace SpecificationPatternRepository.Core.UnitTests.SpecificationBuilderTests
+{
+    public class OrderByDesc
+    {
+        [Fact]
+        public void AddsNothingToList_GivenNoOrderExpression()
+        {
+            // arrange
+            StoreEmptySpec spec = new StoreEmptySpec();
+
+            // assert
+            spec.OrderByClauses.Should().BeEmpty();
+        }
+
+        //[Fact]
+        //public void AddsNothingToList_GivenOrderExpressionWithFalseCondition()
+        //{
+        //    var spec = new CompanyByIdWithFalseConditions(1);
+
+        //    spec.OrderExpressions.Should().BeEmpty();
+        //}
+
+        [Fact]
+        public void AddsOrderExpressionToListWithOrderByDescendingType_GivenOrderByDescendingExpression()
+        {
+            // arrange
+            StoresOrderedDescendingByNameSpec spec = new StoresOrderedDescendingByNameSpec();
+
+            // assert
+            spec.OrderByClauses.Should().ContainSingle();
+            spec.OrderByClauses.Single().OrderType.Should().Be(OrderByType.OrderByDescending);
+        }
+    }
+}
