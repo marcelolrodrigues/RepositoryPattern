e39843b14f8976fa6162aa549a8374f2a103b39a
Marcelo Rodrigues <lrodrigues.marcelo@gmail.com>
2023-05-12 18:31:19 -0300

    SpecificationBuilderTests: OrderByDescThenBy

---
+++ Repository/tests/SpecificationPatternRepository.Core.UnitTests/SpecificationBuilderTests/OrderByThenBy.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using SpecificationPatternRepository.Core.Expressions;
+using SpecificationPatternRepository.Core.UnitTests.Fixture.Specs;
+
+namespace SpecificationPatternRepository.Core.UnitTests.SpecificationBuilderTests
+{
+    public class OrderByThenBy
+    {
+        [Fact]
+        public void AppendOrderExpressionToListWithThenByType_GivenThenByExpression()
+        {
+            // arrange
+            StoresByCompanyOrderedDescByNameThenByIdSpec spec = 
+                new StoresByCompanyOrderedDescByNameThenByIdSpec(1);
+
+            // assert
+            spec.OrderByClauses.Should().HaveCount(2);
+            spec.OrderByClauses.First().OrderType.Should().Be(OrderByType.OrderByDescending);
+            spec.OrderByClauses.Last().OrderType.Should().Be(OrderByType.ThenBy);
+        }
+    }
+}
